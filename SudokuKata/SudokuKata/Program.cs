cd6e23f850f3cd68acdb1138991408f46f20f11e
zh@sysexpand.com <zh@sysexpand.com>
2016-12-07 21:20:26 +0100

    Initial commit.

---
+++ SudokuKata/SudokuKata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SudokuKata
+{
+    class Program
+    {
+        static void Play()
+        {
+            // Implement Sudoku game here
+        }
+
+        static void Main(string[] args)
+        {
+            Play();
+
+            Console.Write("Press ENTER to exit... ");
+            Console.ReadLine();
+        }
+    }
+}
