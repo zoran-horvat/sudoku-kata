ab06d5dbc367cedddc5197a2bf9b90a59ecc99be
zh@sysexpand.com <zh@sysexpand.com>
2016-12-08 19:38:47 +0100

    Added logic to recognize that one digit can only fit one place in a given row/column/block.

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -191,7 +191,7 @@ namespace SudokuKata
 
             // Board is solved at this point.
             // Now pick subset of digits as the starting position.
-            int remainingDigits = 33;
+            int remainingDigits = 30;
             int maxRemovedPerBlock = 6;
             int[,] removedPerBlock = new int[3, 3];
             int[] positions = Enumerable.Range(0, 9 * 9).ToArray();
@@ -314,10 +314,111 @@ namespace SudokuKata
                     state[singleCandidateIndex] = candidate + 1;
                     changeMade = true;
 
-                    Console.WriteLine();
-                    Console.WriteLine("({0}, {1}) can only contain {2}.",
-                                      row + 1, col + 1, candidate + 1);
+                    Console.WriteLine("({0}, {1}) can only contain {2}.", row + 1, col + 1, candidate + 1);
                     Console.WriteLine(string.Join("\n", board.Select(s => new string(s)).ToArray()));
+                    Console.WriteLine();
+                }
+                else
+                {
+                    // Try to find a number which can only appear in one place in a row/column/block
+
+                    List<string> groupDescriptions = new List<string>();
+                    List<int> candidateRowIndices = new List<int>();
+                    List<int> candidateColIndices = new List<int>();
+                    List<int> candidates = new List<int>();
+
+                    for (int digit = 1; digit <= 9; digit++)
+                    {
+                        int mask = 1 << (digit - 1);
+                        for (int cellGroup = 0; cellGroup < 9; cellGroup++)
+                        {
+                            int rowNumberCount = 0;
+                            int indexInRow = 0;
+
+                            int colNumberCount = 0;
+                            int indexInCol = 0;
+
+                            int blockNumberCount = 0;
+                            int indexInBlock = 0;
+
+                            for (int indexInGroup = 0; indexInGroup < 9; indexInGroup++)
+                            {
+                                int rowStateIndex = 9*cellGroup + indexInGroup;
+                                int colStateIndex = 9*indexInGroup + cellGroup;
+                                int blockRowIndex = (cellGroup/3)*3 + indexInGroup/3;
+                                int blockColIndex = (cellGroup%3)*3 + indexInGroup%3;
+                                int blockStateIndex = blockRowIndex*9 + blockColIndex;
+
+                                if ((candidateMasks[rowStateIndex] & mask) != 0)
+                                {
+                                    rowNumberCount += 1;
+                                    indexInRow = indexInGroup;
+                                }
+
+                                if ((candidateMasks[colStateIndex] & mask) != 0)
+                                {
+                                    colNumberCount += 1;
+                                    indexInCol = indexInGroup;
+                                }
+
+                                if ((candidateMasks[blockStateIndex] & mask) != 0)
+                                {
+                                    blockNumberCount += 1;
+                                    indexInBlock = indexInGroup;
+                                }
+                            }
+
+                            if (rowNumberCount == 1)
+                            {
+                                groupDescriptions.Add($"Row #{cellGroup + 1}");
+                                candidateRowIndices.Add(cellGroup);
+                                candidateColIndices.Add(indexInRow);
+                                candidates.Add(digit);
+                            }
+
+                            if (colNumberCount == 1)
+                            {
+                                groupDescriptions.Add($"Column #{cellGroup + 1}");
+                                candidateRowIndices.Add(indexInCol);
+                                candidateColIndices.Add(cellGroup);
+                                candidates.Add(digit);
+                            }
+
+                            if (blockNumberCount == 1)
+                            {
+                                int blockRow = cellGroup/3;
+                                int blockCol = cellGroup%3;
+
+                                groupDescriptions.Add($"Block ({blockRow + 1}, {blockCol + 1})");
+                                candidateRowIndices.Add(blockRow*3 + indexInBlock/3);
+                                candidateColIndices.Add(blockCol*3 + indexInBlock%3);
+                                candidates.Add(digit);
+                            }
+                        } // for (cellGroup = 0..8)
+                    } // for (digit = 1..9)
+
+                    if (candidates.Count > 0)
+                    {
+                        int index = rng.Next(candidates.Count);
+                        string description = groupDescriptions.ElementAt(index);
+                        int row = candidateRowIndices.ElementAt(index);
+                        int col = candidateColIndices.ElementAt(index);
+                        int digit = candidates.ElementAt(index);
+                        int rowToWrite = row + row/3 + 1;
+                        int colToWrite = col + col/3 + 1;
+
+                        string message = $"{description} can contain {digit} only at ({row + 1}, {col + 1}).";
+
+                        int stateIndex = 9*row + col;
+                        state[stateIndex] = digit;
+                        board[rowToWrite][colToWrite] = (char) ('0' + digit);
+
+                        changeMade = true;
+
+                        Console.WriteLine(message);
+                        Console.WriteLine(string.Join("\n", board.Select(s => new string(s)).ToArray()));
+                        Console.WriteLine();
+                    }
                 }
             }
         }
